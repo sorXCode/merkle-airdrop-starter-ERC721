1d934d67b7f831fcbfaccae4cbcfd7a73825d0b8
sorxcode <sorxcode@gmail.com>
2022-04-13 23:54:32 +0100

    fixed MerkleClaimERC721.t.sol tests

--- contracts/src/test/MerkleClaimERC721.t.sol
+++ contracts/src/test/MerkleClaimERC721.t.sol
@@ -9,17 +9,25 @@ import { MerkleClaimERC721Test } from "./utils/MerkleClaimERC721Test.sol"; // Te
 /// @notice MerkleClaimERC721 tests
 /// @author Anish Agnihotri <contact@anishagnihotri.com>
 contract Tests is MerkleClaimERC721Test {
-  /// @notice Allow Alice to claim 100e18 tokens
-  function testAliceClaim() public {
     // Setup correct proof for Alice
-    bytes32[] memory aliceProof = new bytes32[](5);
-    
-    aliceProof[0] = 0x0b3da53306c1495659b5cc9f744ea981c2a3f127b250e5c8caedfaa97c0e676b;
-    aliceProof[1] = 0x3bf2db328f72a7b881246c777705344a7272736afde1cc2753f0948920f8e123;
-    aliceProof[2] = 0xd63c190d9628e0aea5a46c368f46467e98b766ab168992dbe62db8abccad0798;
-    aliceProof[3] = 0x82284e869d05827e4670ce9699ee08d165eacccfa53e56ea0214300eab2f009a;
-    aliceProof[4] = 0xe2cdd186abccbe4745f410c91aee16804f311a566d45c00273f006d9d21e0b6f;
+    bytes32[5] aliceProof = [
+      bytes32(0x0b3da53306c1495659b5cc9f744ea981c2a3f127b250e5c8caedfaa97c0e676b),
+      bytes32(0x3bf2db328f72a7b881246c777705344a7272736afde1cc2753f0948920f8e123),
+      bytes32(0xd63c190d9628e0aea5a46c368f46467e98b766ab168992dbe62db8abccad0798),
+      bytes32(0x82284e869d05827e4670ce9699ee08d165eacccfa53e56ea0214300eab2f009a),
+      bytes32(0xe2cdd186abccbe4745f410c91aee16804f311a566d45c00273f006d9d21e0b6f)
+    ];
 
+  function getAliceProof() internal view returns (bytes32[] memory _aliceProof){
+    _aliceProof = new bytes32[](5);
+    for (uint8 i = 0; i < aliceProof.length; i++) {
+      _aliceProof[i] = aliceProof[i];
+    }
+  }
+    
+
+  /// @notice Allow Alice to claim 1 tokens
+  function testAliceClaim() public {
     // Collect Alice balance of tokens before claim
     uint256 alicePreBalance = ALICE.tokenBalance();
 
@@ -27,10 +35,10 @@ contract Tests is MerkleClaimERC721Test {
     ALICE.claim(
       // Claiming for Alice
       ALICE.ADDRESS(),
-      // 100 tokens
+      // 1 tokens
       "ipfs://unknown",
       // With valid proof
-      aliceProof
+      getAliceProof()
     );
 
     // Collect Alice balance of tokens after claim
@@ -40,107 +48,82 @@ contract Tests is MerkleClaimERC721Test {
     assertEq(alicePostBalance, alicePreBalance + 1);
   }
 
-  // /// @notice Prevent Alice from claiming twice
-  // function testFailAliceClaimTwice() public {
-  //   // Setup correct proof for Alice
-  //   bytes32[] memory aliceProof = new bytes32[](1);
-  //   aliceProof[0] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+  /// @notice Prevent Alice from claiming twice
+  function testFailAliceClaimTwice() public {
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      ALICE.ADDRESS(),
+      // 1 token
+      "ipfs://unknown",
+      // With valid proof
+      getAliceProof()
+    );
 
-  //   // Claim tokens
-  //   ALICE.claim(
-  //     // Claiming for Alice
-  //     address(ALICE),
-  //     // 100 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof
-  //     aliceProof
-  //   );
+    // Claim tokens again
+    ALICE.claim(
+      // Claiming for Alice
+      ALICE.ADDRESS(),
+      // 1 token
+      "ipfs://unknown",
+      // With valid proof
+      getAliceProof()
+    );
+  }
 
-  //   // Claim tokens again
-  //   ALICE.claim(
-  //     // Claiming for Alice
-  //     address(ALICE),
-  //     // 100 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof
-  //     aliceProof
-  //   );
-  // }
+  /// @notice Prevent Alice from claiming with invalid proof
+  function testFailAliceClaimInvalidProof() public {
+    // Setup incorrect proof for Alice
+    bytes32[] memory _aliceProof = new bytes32[](1);
+    _aliceProof[0] = 0xc11ae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
 
-  // /// @notice Prevent Alice from claiming with invalid proof
-  // function testFailAliceClaimInvalidProof() public {
-  //   // Setup incorrect proof for Alice
-  //   bytes32[] memory aliceProof = new bytes32[](1);
-  //   aliceProof[0] = 0xc11ae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+    // Claim tokens
+    ALICE.claim(
+      // Claiming for Alice
+      address(ALICE),
+      // 100 tokens
+      "ipfs://unknown",
+      // With valid proof
+      _aliceProof
+    );
+  }
 
-  //   // Claim tokens
-  //   ALICE.claim(
-  //     // Claiming for Alice
-  //     address(ALICE),
-  //     // 100 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof
-  //     aliceProof
-  //   );
-  // }
+  /// @notice Prevent Bob from claiming
+  function testFailBobClaim() public {
+    // Setup correct proof for Alice
+    bytes32[] memory _aliceProof = new bytes32[](1);
+    _aliceProof[0] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
 
-  // /// @notice Prevent Alice from claiming with invalid amount
-  // function testFailAliceClaimInvalidAmount() public {
-  //   // Setup correct proof for Alice
-  //   bytes32[] memory aliceProof = new bytes32[](1);
-  //   aliceProof[0] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+    // Claim tokens
+    BOB.claim(
+      // Claiming for Bob
+      BOB.ADDRESS(),
+      // 1 token
+      "ipfs://unknown",
+      // With valid proof (for Alice)
+      _aliceProof
+    );
+  }
 
-  //   // Claim tokens
-  //   ALICE.claim(
-  //     // Claiming for Alice
-  //     address(ALICE),
-  //     // Incorrect: 1000 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof (for 100 tokens)
-  //     aliceProof
-  //   );
-  // }
+  /// @notice Let Bob claim on behalf of Alice
+  function testBobClaimForAlice() public {
+    // Collect Alice balance of tokens before claim
+    uint256 alicePreBalance = ALICE.tokenBalance();
 
-  // /// @notice Prevent Bob from claiming
-  // function testFailBobClaim() public {
-  //   // Setup correct proof for Alice
-  //   bytes32[] memory aliceProof = new bytes32[](1);
-  //   aliceProof[0] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
+    // Claim tokens
+    BOB.claim(
+      // Claiming for Alice
+      ALICE.ADDRESS(),
+      // 1 token
+      "ipfs://unknown",
+      // With valid proof (for Alice)
+      getAliceProof()
+    );
 
-  //   // Claim tokens
-  //   BOB.claim(
-  //     // Claiming for Bob
-  //     address(BOB),
-  //     // 100 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof (for Alice)
-  //     aliceProof
-  //   );
-  // }
+    // Collect Alice balance of tokens after claim
+    uint256 alicePostBalance = ALICE.tokenBalance();
 
-  // /// @notice Let Bob claim on behalf of Alice
-  // function testBobClaimForAlice() public {
-  //   // Setup correct proof for Alice
-  //   bytes32[] memory aliceProof = new bytes32[](1);
-  //   aliceProof[0] = 0xceeae64152a2deaf8c661fccd5645458ba20261b16d2f6e090fe908b0ac9ca88;
-
-  //   // Collect Alice balance of tokens before claim
-  //   uint256 alicePreBalance = ALICE.tokenBalance();
-
-  //   // Claim tokens
-  //   BOB.claim(
-  //     // Claiming for Alice
-  //     address(ALICE),
-  //     // 100 tokens
-  //     "ipfs://unknown",
-  //     // With valid proof (for Alice)
-  //     aliceProof
-  //   );
-
-  //   // Collect Alice balance of tokens after claim
-  //   uint256 alicePostBalance = ALICE.tokenBalance();
-
-  //   // Assert Alice balance before + 100 tokens = after balance
-  //   assertEq(alicePostBalance, alicePreBalance + 100e18);
-  // }
+    // Assert Alice balance before + 100 tokens = after balance
+    assertEq(alicePostBalance, alicePreBalance + 1);
+  }
 }
